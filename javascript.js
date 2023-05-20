5217e50b1c0f9e1ecb8f065a37db168924adc2ff
lutricuong <tcuong.lu@gmail.com>
2023-05-20 16:05:11 +0700

    Clean code ver1

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const result = document.querySelector('.result');
+rock.addEventListener('click',playerChooseRock);
+paper.addEventListener('click',playerChoosePaper);
+scissors.addEventListener('click',playerChooseScissors);
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3);
     if (randomNumber === 0) {
@@ -14,74 +24,62 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-const rock = document.querySelector('.rock');
-rock.addEventListener('click',playerRock);
-const paper = document.querySelector('.paper');
-paper.addEventListener('click',playerPaper);
-const scissors = document.querySelector('.scissors');
-scissors.addEventListener('click',playerScissors);
-let playerScore = 0;
-let computerScore = 0;
-result = document.querySelector('.result');
-
-function playerRock() {
-    getComputerChoice();
-    const playerSelection = 'rock';
-    console.log(playerSelection);
-    console.log(computerSelection);
+function rockPaperScissors() {
     if ((playerSelection === 'rock') && (computerSelection ==='paper')) {
         computerScore += 1;
     }
     else if ((playerSelection === 'rock') && (computerSelection ==='scissors')) {
         playerScore += 1;
     }
-    console.log(playerScore);
-    console.log(computerScore);
-    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
-    Score();
-}
-function playerPaper() {
-    getComputerChoice();
-    const playerSelection = 'paper';
-    console.log(playerSelection);
-    console.log(computerSelection);
     if ((playerSelection === 'paper') && (computerSelection ==='rock')) {
         playerScore += 1;
     }
     else if ((playerSelection === 'paper') && (computerSelection ==='scissors')) {
         computerScore += 1;
     }
-    console.log(playerScore);
-    console.log(computerScore);
-    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
-    Score();
-}
-function playerScissors() {
-    getComputerChoice();
-    const playerSelection = 'scissors';
-    console.log(playerSelection);
-    console.log(computerSelection);
     if ((playerSelection === 'scissors') && (computerSelection ==='rock')) {
         computerScore += 1;
     }
     else if ((playerSelection === 'scissors') && (computerSelection ==='paper')) {
         playerScore += 1;
     }
-    console.log(playerScore);
-    console.log(computerScore);
-    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
-    Score();
 }
-function Score() {
+
+function resetScore() {
     if (playerScore === 5) {
         result.textContent = `Player Win!!!`;
         playerScore = 0;
         computerScore = 0;
-}
+    }
     else if (computerScore === 5) {
         result.textContent = `Computer Win!!!`;
         playerScore = 0;
         computerScore = 0;
+    }
 }
+
+function playerChooseRock() {
+    playerSelection = 'rock';
+    getComputerChoice();
+    rockPaperScissors();
+    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
+    resetScore();
 }
+
+function playerChoosePaper() {
+    playerSelection = 'paper';
+    getComputerChoice();
+    rockPaperScissors();
+    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
+    resetScore();
+}
+
+function playerChooseScissors() {
+    playerSelection = 'scissors';
+    getComputerChoice();
+    rockPaperScissors();
+    result.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
+    resetScore();
+}
+
 
